c5044b2c8fbf52eaeb10049d0c6b4e4f05b6c3e1
Joaquín Jaramillo <ioachimj@gmail.com>
2016-03-04 08:08:48 -0300

    Fix whitespace issues by using sax directly

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 ## Clang-Format
 [Clang-Format](http://clang.llvm.org/docs/ClangFormat.html) is a tool to format C/C++/Java/JavaScript/Objective-C/Protobuf code. It can be configured with a config file within the working folder or a parent folder. Configuration see: http://clang.llvm.org/docs/ClangFormatStyleOptions.html
 
+This version is a fork of https://github.com/xaverh/vscode-clang-format-provider. It changes the implementation so it doesn't require saving the file, works by doing partial edits (instead of replacing the whole buffer), and enables range formatting.
+
 ## Usage
 If clang-format is installed and in PATH, C/C++ etc source files can be formatted with Visual Studio Code's built-in formatter (Usually: Ctrl+Shift+F).
 
@@ -11,4 +13,4 @@ Options for the Visual Studio Code extension (eg choice of languages, format at 
 
 ## Source code
 
-Available on github: https://github.com/xaverh/vscode-clang-format-provider
+Available on github: https://github.com/ioachim/vscode-clang-format-provider
--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,7 @@ import cp = require('child_process');
 import path = require('path');
 import {C_MODE, CPP_MODE, OBJECTIVE_C_MODE, JAVA_MODE} from './clangMode';
 import { getBinPath } from './clangPath';
-import parseXml = require('xml-parser');
-import entities = require('entities');  
+import sax = require('sax')
 
 export class ClangDocumentFormattingEditProvider implements vscode.DocumentFormattingEditProvider, vscode.DocumentRangeFormattingEditProvider {
     private formatCommand = 'clang-format';
@@ -19,14 +18,77 @@ export class ClangDocumentFormattingEditProvider implements vscode.DocumentForma
 	*/
 
     public provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
-        return this.doFormatDocument(document, options, token);
+        return this.doFormatDocument(document, null, options, token);
     }
 
     public provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
-        return null;
+        return this.doFormatDocument(document, range, options, token);
     }
 
-    private doFormatDocument(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
+    private getEdits(document: vscode.TextDocument, xml: string): Thenable<vscode.TextEdit[]> {
+        return new Promise((resolve, reject) => {
+            var options = { trim: false, normalize: false, loose: true }
+            var parser = sax.parser(true, options);
+
+            var edits: vscode.TextEdit[] = [];
+            var currentEdit: { length: number, offset: number, text: string };
+
+            parser.onerror = (err) => {
+                reject(err.message);
+            };
+
+
+            parser.onopentag = (tag) => {
+                if (currentEdit) { reject("Malformed output"); }
+
+                switch (tag.name) {
+                    case "replacements":
+                        return;
+
+                    case "replacement":
+                        currentEdit = {
+                            length: parseInt(tag.attributes['length'].toString()),
+                            offset: parseInt(tag.attributes['offset'].toString()),
+                            text: ''
+                        };
+                        break;
+
+                    default:
+                        reject(`Unexpected tag ${tag.name}`);
+                }
+
+
+            };
+
+            parser.ontext = (text) => {
+                if (!currentEdit) { return; }
+
+                currentEdit.text = text;
+            }
+
+            parser.onclosetag = (tagName) => {
+                if (!currentEdit) { return; }
+
+                var start = document.positionAt(currentEdit.offset);
+                var end = document.positionAt(currentEdit.offset + currentEdit.length);
+
+                var editRange = new vscode.Range(start, end);
+               
+                edits.push(new vscode.TextEdit(editRange, currentEdit.text));
+                currentEdit = null;
+            }
+
+            parser.onend = () => {
+                resolve(edits);
+            }
+
+            parser.write(xml);
+            parser.end();
+
+        });
+    }
+
+    private doFormatDocument(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
         return new Promise((resolve, reject) => {
             var filename = document.fileName;
 
@@ -39,44 +101,38 @@ export class ClangDocumentFormattingEditProvider implements vscode.DocumentForma
                         return resolve(null);
                     }
                     if (err) return reject("Cannot format due to syntax errors.");
+
+                    var dummyProcessor = (value: string) => {
+                        debugger;
+                        return value;
+                    };
+                    return resolve(this.getEdits(document, stdout));
                     
-                    var replacements = parseXml(stdout.toString()).root.children;
-                    
-                    var edits = replacements.map((item) => {
-                        var offset = parseInt(item.attributes['offset']);
-                        var length = parseInt(item.attributes['length']);
-                        
-                        var start = document.positionAt(offset);
-                        var end = document.positionAt(offset + length);
-                        
-                        var editRange = new vscode.Range(start, end); 
-                        var text = entities.decodeXML(item.content);
-                       
-                        return new vscode.TextEdit(editRange, text);
-                    });
-                    
-                    // TODO: Should use `-d` option to get a diff and then compute the
-                    // specific edits instead of replace whole buffer
-                    var lastLine = document.lineCount;
-                    var lastLineLastCol = document.lineAt(lastLine - 1).range.end.character;
-                    var range = new vscode.Range(0, 0, lastLine - 1, lastLineLastCol);
-                    return resolve(edits);
                 } catch (e) {
                     reject(e);
                 }
             };
-            
-            var formatArgs = ['--output-replacements-xml'];
-            
+
+            var formatArgs = ['-output-replacements-xml'];
+
+            if (range) {
+                var offset = document.offsetAt(range.start);
+                var length = document.offsetAt(range.end) - offset;
+
+                formatArgs.push(`-offset=${offset}`, `-length=${length}`)
+            }
+
             var child = cp.execFile(formatCommandBinPath, formatArgs, {}, childCompleted);
             child.stdin.end(document.getText());
-            
+
             token.onCancellationRequested(() => {
                 child.kill();
-                reject("Cancelation requested"); 
+                reject("Cancelation requested");
             });
-            
-            
+
+
+
+
         });
     }
 
@@ -86,12 +142,10 @@ let diagnosticCollection: vscode.DiagnosticCollection;
 
 export function activate(ctx: vscode.ExtensionContext): void {
 
-    ctx.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(CPP_MODE, new ClangDocumentFormattingEditProvider()));
-    ctx.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(C_MODE, new ClangDocumentFormattingEditProvider()));
-    ctx.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(JAVA_MODE, new ClangDocumentFormattingEditProvider()));
-    ctx.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(OBJECTIVE_C_MODE, new ClangDocumentFormattingEditProvider()));
+    var formatter = new ClangDocumentFormattingEditProvider();
 
-}
-
-function deactivate() {
+    [C_MODE, CPP_MODE, JAVA_MODE, OBJECTIVE_C_MODE].forEach(mode => {
+        ctx.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider(mode, formatter));
+        ctx.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(mode, formatter));
+    })
 }
