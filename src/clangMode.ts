73dcda5b27de96e237a2e76cbae8778132cd082c
Evan Martin <martine@danga.com>
2016-06-16 12:31:32 -0700

    add javascript/typescript to the languages list
    Fixes issue #11.

--- src/clangMode.ts
+++ src/clangMode.ts
@@ -2,5 +2,5 @@
 
 import vscode = require('vscode');
 
-const LANGUAGES: string[] = ['cpp', 'c', 'objective-c', 'java'];
+const LANGUAGES: string[] = ['cpp', 'c', 'objective-c', 'java', 'javascript', 'typescript'];
 export const MODES: vscode.DocumentFilter[] = LANGUAGES.map(language => ({ language, scheme: 'file' }));
