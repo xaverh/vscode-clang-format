49716fbeb182299be8a7ff3fff39402e251a80d2
Xaver Hellauer <xaver@hellauer.bayern>
2017-04-20 14:28:40 +0200

    basic linting and formatting

--- src/clangPath.ts
+++ src/clangPath.ts
@@ -3,39 +3,41 @@
 import fs = require('fs');
 import path = require('path');
 
-var binPathCache: { [bin: string]: string; } = {}
+var binPathCache: {[bin: string] : string;} = {}
 
 export function getBinPath(binname: string) {
-	if (binPathCache[binname]) return binPathCache[binname];
+  if (binPathCache[binname]) {
+    return binPathCache[binname]
+  };
 
-	for (let binNameToSearch of correctBinname(binname)) {
-		// clang-format.executable has a valid absolute path
-		if (fs.existsSync(binNameToSearch)) {
-			binPathCache[binname] = binNameToSearch;
-			return binNameToSearch;
-		}
+  for (let binNameToSearch of correctBinname(binname)) {
+    // clang-format.executable has a valid absolute path
+    if (fs.existsSync(binNameToSearch)) {
+      binPathCache[binname] = binNameToSearch;
+      return binNameToSearch;
+    }
 
-		if (process.env["PATH"]) {
-			var pathparts = process.env["PATH"].split(path.delimiter);
-			for (var i = 0; i < pathparts.length; i++) {
-				let binpath = path.join(pathparts[i], binNameToSearch);
-				if (fs.existsSync(binpath)) {
-					binPathCache[binname] = binpath;
-					return binpath;
-				}
-			}
-		}
-	}
+    if (process.env['PATH']) {
+      var pathparts = process.env['PATH'].split(path.delimiter);
+      for (var i = 0; i < pathparts.length; i++) {
+        let binpath = path.join(pathparts[i], binNameToSearch);
+        if (fs.existsSync(binpath)) {
+          binPathCache[binname] = binpath;
+          return binpath;
+        }
+      }
+    }
+  }
 
-	// Else return the binary name directly (this will likely always fail downstream) 
-	binPathCache[binname] = binname;
-	return binname;
+  // Else return the binary name directly (this will likely always fail downstream)
+  binPathCache[binname] = binname;
+  return binname;
 }
 
-function correctBinname(binname: string): [string] {
-	if (process.platform === 'win32') {
-        return [binname + '.exe', binname + '.bat', binname + '.cmd', binname];
-    } else {
-		return [binname]
-    }
+function correctBinname(binname: string): [ string ] {
+  if (process.platform === 'win32') {
+    return [ binname + '.exe', binname + '.bat', binname + '.cmd', binname ];
+  } else {
+    return [ binname ];
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,250 +1,251 @@
 import * as vscode from 'vscode';
 import cp = require('child_process');
 import path = require('path');
-import { MODES } from './clangMode';
-import { getBinPath } from './clangPath';
+import {MODES} from './clangMode';
+import {getBinPath} from './clangPath';
 import sax = require('sax');
 
 export class ClangDocumentFormattingEditProvider implements vscode.DocumentFormattingEditProvider, vscode.DocumentRangeFormattingEditProvider {
-    private defaultConfigure = {
-        executable: 'clang-format',
-        style: 'file',
-        fallbackStyle: 'none',
-        assumeFilename: ''
-    };
+  private defaultConfigure = {
+    executable : 'clang-format',
+    style : 'file',
+    fallbackStyle : 'none',
+    assumeFilename : ''
+  };
 
-    public provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
-        return this.doFormatDocument(document, null, options, token);
-    }
+  public provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
+    return this.doFormatDocument(document, null, options, token);
+  }
 
-    public provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
-        return this.doFormatDocument(document, range, options, token);
-    }
+  public provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
+    return this.doFormatDocument(document, range, options, token);
+  }
 
-    private getEdits(document: vscode.TextDocument, xml: string, codeContent: string): Thenable<vscode.TextEdit[]> {
-        return new Promise((resolve, reject) => {
-            var options = { trim: false, normalize: false, loose: true }
-            var parser = sax.parser(true, options);
+  private getEdits(document: vscode.TextDocument, xml: string, codeContent: string): Thenable<vscode.TextEdit[]> {
+    return new Promise((resolve, reject) => {
+      var options = {
+        trim : false,
+        normalize : false,
+        loose : true
+      };
+      var parser = sax.parser(true, options);
 
-            var edits: vscode.TextEdit[] = [];
-            var currentEdit: { length: number, offset: number, text: string };
+      var edits: vscode.TextEdit[] = [];
+      var currentEdit: {length : number, offset : number, text : string};
 
-            var codeBuffer = new Buffer(codeContent);
-            // encoding position cache
-            var codeByteOffsetCache = {
-                byte: 0,
-                offset: 0
-            };
-            var byteToOffset = function (editInfo: { length: number, offset: number }) {
-                var offset = editInfo.offset;
-                var length = editInfo.length;
+      var codeBuffer = new Buffer(codeContent);
+      // encoding position cache
+      var codeByteOffsetCache = {
+        byte : 0,
+        offset : 0
+      };
+      var byteToOffset = function(editInfo: {length : number, offset : number}) {
+        var offset = editInfo.offset;
+        var length = editInfo.length;
 
-                if (offset >= codeByteOffsetCache.byte) {
-                    editInfo.offset = codeByteOffsetCache.offset + codeBuffer.slice(codeByteOffsetCache.byte, offset).toString('utf8').length;
-                    codeByteOffsetCache.byte = offset;
-                    codeByteOffsetCache.offset = editInfo.offset;
-                } else {
-                    editInfo.offset = codeBuffer.slice(0, offset).toString('utf8').length;
-                    codeByteOffsetCache.byte = offset;
-                    codeByteOffsetCache.offset = editInfo.offset;
-                }
-
-                editInfo.length = codeBuffer.slice(offset, offset + length).toString('utf8').length;
-
-                return editInfo;
-            };
-
-            parser.onerror = (err) => {
-                reject(err.message);
-            };
-
-
-            parser.onopentag = (tag) => {
-                if (currentEdit) {
-                    reject("Malformed output");
-                }
-
-                switch (tag.name) {
-                    case "replacements":
-                        return;
-
-                    case "replacement":
-                        currentEdit = {
-                            length: parseInt(tag.attributes['length'].toString()),
-                            offset: parseInt(tag.attributes['offset'].toString()),
-                            text: ''
-                        };
-                        byteToOffset(currentEdit);
-                        break;
-
-                    default:
-                        reject(`Unexpected tag ${tag.name}`);
-                }
-
-
-            };
-
-            parser.ontext = (text) => {
-                if (!currentEdit) { return; }
-
-                currentEdit.text = text;
-            }
-
-            parser.onclosetag = (tagName) => {
-                if (!currentEdit) { return; }
-
-                var start = document.positionAt(currentEdit.offset);
-                var end = document.positionAt(currentEdit.offset + currentEdit.length);
-
-                var editRange = new vscode.Range(start, end);
-
-                edits.push(new vscode.TextEdit(editRange, currentEdit.text));
-                currentEdit = null;
-            }
-
-            parser.onend = () => {
-                resolve(edits);
-            }
-
-            parser.write(xml);
-            parser.end();
-
-        });
-    }
-
-    /// Get execute name in clang-format.executable, if not found, use default value
-    /// If configure has changed, it will get the new value
-    private getExecutablePath() {
-        let execPath = vscode.workspace.getConfiguration('clang-format').get<string>('executable');
-        if (!execPath) {
-            return this.defaultConfigure.executable;
-        }
-
-        // replace placeholders, if present
-        return execPath
-            .replace(/\${workspaceRoot}/g, vscode.workspace.rootPath)
-            .replace(/\${cwd}/g, process.cwd())
-            .replace(/\${env\.([^}]+)}/g, (sub: string, envName: string) => {
-                return process.env[envName];
-            });
-    }
-
-    private getStyle(document: vscode.TextDocument) {
-        let ret = vscode.workspace.getConfiguration('clang-format').get<string>(`language.${document.languageId}.style`);
-        if (ret.trim()) {
-            return ret.trim();
-        }
-
-        ret = vscode.workspace.getConfiguration('clang-format').get<string>('style');
-        if (ret && ret.trim()) {
-            return ret.trim();
+        if (offset >= codeByteOffsetCache.byte) {
+          editInfo.offset = codeByteOffsetCache.offset + codeBuffer.slice(codeByteOffsetCache.byte, offset).toString('utf8').length;
+          codeByteOffsetCache.byte = offset;
+          codeByteOffsetCache.offset = editInfo.offset;
         } else {
-            return this.defaultConfigure.style;
+          editInfo.offset = codeBuffer.slice(0, offset).toString('utf8').length;
+          codeByteOffsetCache.byte = offset;
+          codeByteOffsetCache.offset = editInfo.offset;
         }
+
+        editInfo.length = codeBuffer.slice(offset, offset + length).toString('utf8').length;
+
+        return editInfo;
+      };
+
+      parser.onerror = (err) => {
+        reject(err.message);
+      };
+
+      parser.onopentag = (tag) => {
+        if (currentEdit) {
+          reject('Malformed output');
+        }
+
+        switch (tag.name) {
+        case 'replacements':
+          return;
+
+        case 'replacement':
+          currentEdit = {
+            length : parseInt(tag.attributes['length'].toString()),
+            offset : parseInt(tag.attributes['offset'].toString()),
+            text : ''
+          };
+          byteToOffset(currentEdit);
+          break;
+
+        default:
+          reject(`Unexpected tag ${tag.name}`);
+        }
+
+      };
+
+      parser.ontext = (text) => {
+        if (!currentEdit) { return; }
+
+        currentEdit.text = text;
+      };
+
+      parser.onclosetag = (tagName) => {
+        if (!currentEdit) { return; }
+
+        var start = document.positionAt(currentEdit.offset);
+        var end = document.positionAt(currentEdit.offset + currentEdit.length);
+
+        var editRange = new vscode.Range(start, end);
+
+        edits.push(new vscode.TextEdit(editRange, currentEdit.text));
+        currentEdit = null;
+      };
+
+      parser.onend = () => {
+        resolve(edits);
+      };
+
+      parser.write(xml);
+      parser.end();
+
+    });
+  }
+
+  /// Get execute name in clang-format.executable, if not found, use default value
+  /// If configure has changed, it will get the new value
+  private getExecutablePath() {
+    let execPath = vscode.workspace.getConfiguration('clang-format').get<string>('executable');
+    if (!execPath) {
+      return this.defaultConfigure.executable;
     }
 
-    private getFallbackStyle(document: vscode.TextDocument) {
-        let strConf = vscode.workspace.getConfiguration('clang-format').get<string>(`language.${document.languageId}.fallbackStyle`);
-        if (strConf.trim()) {
-            return strConf;
-        }
+    // replace placeholders, if present
+    return execPath
+      .replace(/\${workspaceRoot}/g, vscode.workspace.rootPath)
+      .replace(/\${cwd}/g, process.cwd())
+      .replace(/\${env\.([^}]+)}/g, (sub: string, envName: string) => {
+        return process.env[envName];
+      });
+  }
 
-        strConf = vscode.workspace.getConfiguration('clang-format').get<string>('fallbackStyle');
-        if (strConf.trim()) {
-            return strConf;
-        }
-
-        return this.defaultConfigure.style;
+  private getStyle(document: vscode.TextDocument) {
+    let ret = vscode.workspace.getConfiguration('clang-format').get<string>(`language.${document.languageId}.style`);
+    if (ret.trim()) {
+      return ret.trim();
     }
 
-    private getAssumedFilename(document: vscode.TextDocument) {
-        let assumedFilename = vscode.workspace.getConfiguration('clang-format').get<string>('assumeFilename');
-        if (assumedFilename === "") {
-            return document.fileName;
-        }
-        return assumedFilename;
+    ret = vscode.workspace.getConfiguration('clang-format').get<string>('style');
+    if (ret && ret.trim()) {
+      return ret.trim();
+    } else {
+      return this.defaultConfigure.style;
+    }
+  }
+
+  private getFallbackStyle(document: vscode.TextDocument) {
+    let strConf = vscode.workspace.getConfiguration('clang-format').get<string>(`language.${document.languageId}.fallbackStyle`);
+    if (strConf.trim()) {
+      return strConf;
     }
 
-    private doFormatDocument(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
-        return new Promise((resolve, reject) => {
-            var filename = document.fileName;
+    strConf = vscode.workspace.getConfiguration('clang-format').get<string>('fallbackStyle');
+    if (strConf.trim()) {
+      return strConf;
+    }
 
-            var formatCommandBinPath = getBinPath(this.getExecutablePath());
-            var codeContent = document.getText();
+    return this.defaultConfigure.style;
+  }
 
-            var childCompleted = (err, stdout, stderr) => {
-                try {
-                    if (err && (<any>err).code == "ENOENT") {
-                        vscode.window.showInformationMessage("The '" + formatCommandBinPath + "' command is not available.  Please check your clang-format.executable user setting and ensure it is installed.");
-                        return resolve(null);
-                    }
-                    if (err) {
-                        return reject("Cannot format due to syntax errors.");
-                    }
+  private getAssumedFilename(document: vscode.TextDocument) {
+    let assumedFilename = vscode.workspace.getConfiguration('clang-format').get<string>('assumeFilename');
+    if (assumedFilename === '') {
+      return document.fileName;
+    }
+    return assumedFilename;
+  }
 
-                    var dummyProcessor = (value: string) => {
-                        debugger;
-                        return value;
-                    };
-                    return resolve(this.getEdits(document, stdout, codeContent));
+  private doFormatDocument(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
+    return new Promise((resolve, reject) => {
+      var filename = document.fileName;
 
-                } catch (e) {
-                    reject(e);
-                }
-            };
+      var formatCommandBinPath = getBinPath(this.getExecutablePath());
+      var codeContent = document.getText();
 
-            var formatArgs = [
-                '-output-replacements-xml',
-                `-style=${this.getStyle(document)}`,
-                `-fallback-style=${this.getFallbackStyle(document)}`,
-                `-assume-filename=${this.getAssumedFilename(document)}`,
-            ];
+      var childCompleted = (err, stdout, stderr) => {
+        try {
+          if (err && (<any>err).code === 'ENOENT') {
+            vscode.window.showInformationMessage('The \'' + formatCommandBinPath + '\' command is not available.  Please check your clang-format.executable user setting and ensure it is installed.');
+            return resolve(null);
+          }
+          if (err) {
+            return reject('Cannot format due to syntax errors.');
+          }
 
-            if (range) {
-                var offset = document.offsetAt(range.start);
-                var length = document.offsetAt(range.end) - offset;
+          var dummyProcessor = (value: string) => {
+            debugger;
+            return value;
+          };
+          return resolve(this.getEdits(document, stdout, codeContent));
 
-                // fix charater length to byte length
-                length = Buffer.byteLength(codeContent.substr(offset, length), 'utf8');
-                // fix charater offset to byte offset
-                offset = Buffer.byteLength(codeContent.substr(0, offset), 'utf8');
+        } catch (e) {
+          reject(e);
+        }
+      };
 
-                formatArgs.push(`-offset=${offset}`, `-length=${length}`)
-            }
+      var formatArgs = [
+        '-output-replacements-xml',
+        `-style=${this.getStyle(document)}`,
+        `-fallback-style=${this.getFallbackStyle(document)}`,
+        `-assume-filename=${this.getAssumedFilename(document)}`,
+      ];
 
-            var workingPath = vscode.workspace.rootPath;
-            if (!document.isUntitled) {
-                workingPath = path.dirname(document.fileName);
-            }
+      if (range) {
+        var offset = document.offsetAt(range.start);
+        var length = document.offsetAt(range.end) - offset;
 
-            var child = cp.execFile(formatCommandBinPath, formatArgs, { cwd: workingPath }, childCompleted);
-            child.stdin.end(codeContent);
+        // fix charater length to byte length
+        length = Buffer.byteLength(codeContent.substr(offset, length), 'utf8');
+        // fix charater offset to byte offset
+        offset = Buffer.byteLength(codeContent.substr(0, offset), 'utf8');
 
-            if (token) {
-                token.onCancellationRequested(() => {
-                    child.kill();
-                    reject("Cancelation requested");
-                });
-            }
+        formatArgs.push(`-offset=${offset}`, `-length=${length}`);
+      }
+
+      var workingPath = vscode.workspace.rootPath;
+      if (!document.isUntitled) {
+        workingPath = path.dirname(document.fileName);
+      }
+
+      var child = cp.execFile(formatCommandBinPath, formatArgs, {cwd : workingPath}, childCompleted);
+      child.stdin.end(codeContent);
+
+      if (token) {
+        token.onCancellationRequested(() => {
+          child.kill();
+          reject('Cancelation requested');
         });
-    }
+      }
+    });
+  }
 
-    public formatDocument(document: vscode.TextDocument): Thenable<vscode.TextEdit[]> {
-        return this.doFormatDocument(document, null, null, null);
-    }
-
+  public formatDocument(document: vscode.TextDocument): Thenable<vscode.TextEdit[]> {
+    return this.doFormatDocument(document, null, null, null);
+  }
 }
 
 let diagnosticCollection: vscode.DiagnosticCollection;
 
 export function activate(ctx: vscode.ExtensionContext): void {
 
-    var formatter = new ClangDocumentFormattingEditProvider();
-    var availableLanguages = {};
+  var formatter = new ClangDocumentFormattingEditProvider();
+  var availableLanguages = {};
 
-    MODES.forEach(mode => {
-        ctx.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider(mode, formatter));
-        ctx.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(mode, formatter));
-        availableLanguages[mode.language] = true;
-    });
+  MODES.forEach(mode => {
+    ctx.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider(mode, formatter));
+    ctx.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(mode, formatter));
+    availableLanguages[mode.language] = true;
+  });
 }
